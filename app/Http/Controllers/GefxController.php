0630d250faf9fb4419cf3f06d396ea3392295195
bluesky2008 <william_billl2008@yahoo.co.jp>
2022-07-10 17:58:58 +0900

    viewsディレクトリ直下にv2ディレクトリを追加・移動

--- app/Http/Controllers/GefxController.php
+++ app/Http/Controllers/GefxController.php
@@ -10,73 +10,73 @@ class GefxController extends Controller
 {
     public function index()
     {
-        return view('index');
+        return view('v2/index');
     }
 
     public function faq()
     {
-        return view('faq');
+        return view('v2/faq');
     }
 
     public function new_acount()
     {
-        return view('new_acount');
+        return view('v2/new_acount');
     }
 
     public function demo_acount()
     {
-        return view('demo_acount');
+        return view('v2/demo_acount');
     }
 
     public function login()
     {
-        return view('login');
+        return view('v2/login');
     }
 
     public function company()
     {
-        return view('company');
+        return view('v2/company');
     }
 
     public function contact()
     {
-        return view('contact');
+        return view('v2/contact');
     }
 
 
     public function summary()
     {
-        return view('summary');
+        return view('v2/summary');
     }
 
     public function history()
     {
-        return view('history');
+        return view('v2/history');
     }
 
     public function deposit()
     {
-        return view('deposit');
+        return view('v2/deposit');
     }
 
     public function transfer()
     {
-        return view('transfer');
+        return view('v2/transfer');
     }
 
     public function add_acount()
     {
-        return view('add_acount');
+        return view('v2/add_acount');
     }
 
     public function setting()
     {
-        return view('setting');
+        return view('v2/setting');
     }
 
     public function setting2()
     {
-        return view('setting2');
+        return view('v2/setting2');
     }
 
 
