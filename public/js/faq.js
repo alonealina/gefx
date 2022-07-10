d4579e8208c03f76950513ba79966be5f491c4e1
bluesky2008 <william_billl2008@yahoo.co.jp>
2022-07-10 19:25:58 +0900

    FAQ実装

---
+++ public/js/faq.js
@@ -0,0 +1,109 @@
+let q1 = document.getElementById("faq_q1");
+let q2 = document.getElementById("faq_q2");
+let q3 = document.getElementById("faq_q3");
+let q4 = document.getElementById("faq_q4");
+let q5 = document.getElementById("faq_q5");
+let q6 = document.getElementById("faq_q6");
+let q7 = document.getElementById("faq_q7");
+let q8 = document.getElementById("faq_q8");
+let q9 = document.getElementById("faq_q9");
+let a1 = document.getElementById("faq_a1");
+let a2 = document.getElementById("faq_a2");
+let a3 = document.getElementById("faq_a3");
+let a4 = document.getElementById("faq_a4");
+let a5 = document.getElementById("faq_a5");
+let a6 = document.getElementById("faq_a6");
+let a7 = document.getElementById("faq_a7");
+let a8 = document.getElementById("faq_a8");
+let a9 = document.getElementById("faq_a9");
+
+q1.addEventListener("click", function () {
+    if (a1.classList.contains("faq_close")) {
+        a1.classList.remove("faq_close");
+        a1.classList.add("faq_open");
+    } else {
+        a1.classList.remove("faq_open");
+        a1.classList.add("faq_close");
+    }
+});
+
+q2.addEventListener("click", function () {
+    if (a2.classList.contains("faq_close")) {
+        a2.classList.remove("faq_close");
+        a2.classList.add("faq_open");
+    } else {
+        a2.classList.remove("faq_open");
+        a2.classList.add("faq_close");
+    }
+});
+
+q3.addEventListener("click", function () {
+    if (a3.classList.contains("faq_close")) {
+        a3.classList.remove("faq_close");
+        a3.classList.add("faq_open");
+    } else {
+        a3.classList.remove("faq_open");
+        a3.classList.add("faq_close");
+    }
+});
+
+q4.addEventListener("click", function () {
+    if (a4.classList.contains("faq_close")) {
+        a4.classList.remove("faq_close");
+        a4.classList.add("faq_open");
+    } else {
+        a4.classList.remove("faq_open");
+        a4.classList.add("faq_close");
+    }
+});
+
+q5.addEventListener("click", function () {
+    if (a5.classList.contains("faq_close")) {
+        a5.classList.remove("faq_close");
+        a5.classList.add("faq_open");
+    } else {
+        a5.classList.remove("faq_open");
+        a5.classList.add("faq_close");
+    }
+});
+
+q6.addEventListener("click", function () {
+    if (a6.classList.contains("faq_close")) {
+        a6.classList.remove("faq_close");
+        a6.classList.add("faq_open");
+    } else {
+        a6.classList.remove("faq_open");
+        a6.classList.add("faq_close");
+    }
+});
+
+q7.addEventListener("click", function () {
+    if (a7.classList.contains("faq_close")) {
+        a7.classList.remove("faq_close");
+        a7.classList.add("faq_open");
+    } else {
+        a7.classList.remove("faq_open");
+        a7.classList.add("faq_close");
+    }
+});
+
+q8.addEventListener("click", function () {
+    if (a8.classList.contains("faq_close")) {
+        a8.classList.remove("faq_close");
+        a8.classList.add("faq_open");
+    } else {
+        a8.classList.remove("faq_open");
+        a8.classList.add("faq_close");
+    }
+});
+
+q9.addEventListener("click", function () {
+    if (a9.classList.contains("faq_close")) {
+        a9.classList.remove("faq_close");
+        a9.classList.add("faq_open");
+    } else {
+        a9.classList.remove("faq_open");
+        a9.classList.add("faq_close");
+    }
+});
+
--- resources/views/v2/faq.blade.php
+++ resources/views/v2/faq.blade.php
@@ -6,22 +6,31 @@
     <img src="{{ asset('img/logo2.png') }}" class="head_logo" alt="">
     <div class="head_title">よくあるご質問</div>
     <div class="faq_div">
-        <div class="faq_q">1. アカウントへの入出金はどうすればよいですか？</div>
-        <div class="faq_a">いくつかの方法を使用してアカウントに入出金を行うことができます。会員ページの入出金ページをご覧ください。</div>
-        <div class="faq_q">2. 口座から別の口座に資金を送金するにはどうすればよいですか？</div>
-        <div class="faq_q">3. 私の資金は安全で分散されていますか？</div>
-        <div class="faq_q">4. デモアカウントを提供していますか？</div>
-        <div class="faq_q">5. どのプラットフォームを提供していますか？</div>
-        <div class="faq_q">6. 暗号資産取引を提供していますか？</div>
-        <div class="faq_q">7. GOOD ENOUGH FXの流動性プロバイダーは誰ですか？</div>
-        <div class="faq_q">8. レバレッジ設定が変更されることはありますか？</div>
-        <div class="faq_q">9. 入金が反映されるまで、どれくらい時間がかかりますか？</div>
+        <div id="faq_q1" class="faq_q">1. アカウントへの入出金はどうすればよいですか？</div>
+        <div id="faq_a1" class="faq_a faq_close">いくつかの方法を使用してアカウントに入出金を行うことができます。会員ページの入出金ページをご覧ください。</div>
+        <div id="faq_q2" class="faq_q">2. 口座から別の口座に資金を送金するにはどうすればよいですか？</div>
+        <div id="faq_a2" class="faq_a faq_close">会員ページの資金移動ページから可能です。</div>
+        <div id="faq_q3" class="faq_q">3. 私の資金は安全で分散されていますか？</div>
+        <div id="faq_a3" class="faq_a faq_close">はい、すべての資金は分散された口座、ウォレットに保管されており、企業の資金とは完全に分離されており、混合されていません。</div>
+        <div id="faq_q4" class="faq_q">4. デモアカウントを提供していますか？</div>
+        <div id="faq_a4" class="faq_a faq_close">はい、ここでデモアカウントにサインアップできます：<a href="{{ route('v2_demo_acount') }}">デモ口座開設ページ</a></div>
+        <div id="faq_q5" class="faq_q">5. どのプラットフォームを提供していますか？</div>
+        <div id="faq_a5" class="faq_a faq_close">MetaTrader 4を提供しています。将来的にMetaTrader5、cTrader、Currenex（FXのみ）を含む、世界をリードするいくつかの異なるプラットフォームを提供しています。<br>SterlingTraderとの交換株式も提供していきます。</div>
+        <div id="faq_q6" class="faq_q">6. 暗号資産取引を提供していますか？</div>
+        <div id="faq_a6" class="faq_a faq_close">はい、さまざまな暗号通貨ペアを提供しています。スプレッドと実行を表示するには、当社のプラットフォームをダウンロードしてください。</div>
+        <div id="faq_q7" class="faq_q">7. GOOD ENOUGH FXの流動性プロバイダーは誰ですか？</div>
+        <div id="faq_a7" class="faq_a faq_close">さまざまなLPとの緊密な関係を通じて、市場で最もタイトなスプレッドのいくつかを提供しています。価格フィードを集計して、クライアントの最上位の価格を示す、いつでも最高のBID / OFFERを表示します。</div>
+        <div id="faq_q8" class="faq_q">8. レバレッジ設定が変更されることはありますか？</div>
+        <div id="faq_a8" class="faq_a faq_close">レバレッジ設定に関してはお客様ご自身で選択いただけます。</div>
+        <div id="faq_q9" class="faq_q">9. 入金が反映されるまで、どれくらい時間がかかりますか？</div>
+        <div id="faq_a9" class="faq_a faq_close">ご入金が反映されるまで、最大3営業日程度時間を要します。</div>
     </div>
     <a href="{{ route('v2_contact') }}" class="btn_a">
         <div class="btn_purple" style="margin-top:50px;">お問い合わせはこちら</div>
     </a>
 </div>
 
+<script src="{{ asset('js/faq.js') }}"></script>
 @endsection
 
 
@@ -33,16 +42,16 @@
     <img src="{{ asset('img/logo2.png') }}" class="head_logo" alt="">
     <div class="head_title">よくあるご質問</div>
     <div class="acount_div_sp">
-        <div class="faq_q">1. アカウントへの入出金はどうすればよいですか？</div>
-        <div class="faq_a">いくつかの方法を使用してアカウントに入出金を行うことができます。会員ページの入出金ページをご覧ください。</div>
-        <div class="faq_q">2. 口座から別の口座に資金を送金するにはどうすればよいですか？</div>
-        <div class="faq_q">3. 私の資金は安全で分散されていますか？</div>
-        <div class="faq_q">4. デモアカウントを提供していますか？</div>
-        <div class="faq_q">5. どのプラットフォームを提供していますか？</div>
-        <div class="faq_q">6. 暗号資産取引を提供していますか？</div>
-        <div class="faq_q">7. GOOD ENOUGH FXの流動性プロバイダーは誰ですか？</div>
-        <div class="faq_q">8. レバレッジ設定が変更されることはありますか？</div>
-        <div class="faq_q">9. 入金が反映されるまで、どれくらい時間がかかりますか？</div>
+        <div id="faq_q1" class="faq_q">1. アカウントへの入出金はどうすればよいですか？</div>
+        <div id="faq_a1" class="faq_a faq_close">いくつかの方法を使用してアカウントに入出金を行うことができます。会員ページの入出金ページをご覧ください。</div>
+        <div id="faq_q2" class="faq_q">2. 口座から別の口座に資金を送金するにはどうすればよいですか？</div>
+        <div id="faq_q3" class="faq_q">3. 私の資金は安全で分散されていますか？</div>
+        <div id="faq_q4" class="faq_q">4. デモアカウントを提供していますか？</div>
+        <div id="faq_q5" class="faq_q">5. どのプラットフォームを提供していますか？</div>
+        <div id="faq_q6" class="faq_q">6. 暗号資産取引を提供していますか？</div>
+        <div id="faq_q7" class="faq_q">7. GOOD ENOUGH FXの流動性プロバイダーは誰ですか？</div>
+        <div id="faq_q8" class="faq_q">8. レバレッジ設定が変更されることはありますか？</div>
+        <div id="faq_q9" class="faq_q">9. 入金が反映されるまで、どれくらい時間がかかりますか？</div>
     </div>
     <a href="{{ route('v2_contact') }}" class="btn_sp_a">
         <div class="btn_sp" style="margin-top:50px;">お問い合わせはこちら</div>
