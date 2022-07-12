6739072fa4640e5cf480aa0b7acffedaaf81e540
bluesky2008 <william_billl2008@yahoo.co.jp>
2022-07-12 14:58:14 +0900

    コントローラをv2に移動

--- app/Http/Controllers/v2/Controller.php
+++ app/Http/Controllers/v2/Controller.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\v2;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\v2\V2IndexController;
+use App\Http\Controllers\v2\V2NewAcountController;
+use App\Http\Controllers\v2\V2DemoAcountController;
+use App\Http\Controllers\v2\V2LoginController;
+use App\Http\Controllers\v2\V2ContactController;
+use App\Http\Controllers\v2\V2FaqController;
+use App\Http\Controllers\v2\V2UserController;
 
-Route::get('v2/', 'App\Http\Controllers\V2IndexController@index')->name('v2_index');
-Route::get('v2/new_acount', 'App\Http\Controllers\V2NewAcountController@new_acount')->name('v2_new_acount');
-Route::get('v2/demo_acount', 'App\Http\Controllers\V2DemoAcountController@demo_acount')->name('v2_demo_acount');
-Route::get('v2/login', 'App\Http\Controllers\V2LoginController@login')->name('v2_login');
-Route::get('v2/login2', 'App\Http\Controllers\V2LoginController@login2')->name('v2_login2');
-Route::get('v2/contact', 'App\Http\Controllers\V2ContactController@contact')->name('v2_contact');
-Route::get('v2/company', 'App\Http\Controllers\V2CompanyController@company')->name('v2_company');
-Route::get('v2/faq', 'App\Http\Controllers\V2FaqController@faq')->name('v2_faq');
 
-Route::get('v2/summary', 'App\Http\Controllers\V2UserController@summary')->name('v2_summary');
-Route::get('v2/history', 'App\Http\Controllers\V2UserController@history')->name('v2_history');
-Route::get('v2/deposit', 'App\Http\Controllers\V2UserController@deposit')->name('v2_deposit');
-Route::get('v2/transfer', 'App\Http\Controllers\V2UserController@transfer')->name('v2_transfer');
-Route::get('v2/add_acount', 'App\Http\Controllers\V2UserController@add_acount')->name('v2_add_acount');
-Route::get('v2/setting', 'App\Http\Controllers\V2UserController@setting')->name('v2_setting');
-Route::get('v2/setting2', 'App\Http\Controllers\V2UserController@setting2')->name('v2_setting2');
+Route::get('v2/', [V2IndexController::class, 'index'])->name('v2_index');
+Route::get('v2/new_acount', [V2NewAcountController::class, 'new_acount'])->name('v2_new_acount');
+Route::get('v2/demo_acount', [V2DemoAcountController::class, 'demo_acount'])->name('v2_demo_acount');
+Route::get('v2/login', [V2LoginController::class, 'login'])->name('v2_login');
+Route::get('v2/login2', [V2LoginController::class, 'login2'])->name('v2_login2');
+Route::get('v2/contact', [V2ContactController::class, 'contact'])->name('v2_contact');
+Route::get('v2/company', [V2CompanyController::class, 'company'])->name('v2_company');
+Route::get('v2/faq', [V2FaqController::class, 'faq'])->name('v2_faq');
+
+Route::get('v2/summary', [V2UserController::class, 'summary'])->name('v2_summary');
+Route::get('v2/history', [V2UserController::class, 'history'])->name('v2_history');
+Route::get('v2/deposit', [V2UserController::class, 'deposit'])->name('v2_deposit');
+Route::get('v2/transfer', [V2UserController::class, 'transfer'])->name('v2_transfer');
+Route::get('v2/add_acount', [V2UserController::class, 'add_acount'])->name('v2_add_acount');
+Route::get('v2/setting', [V2UserController::class, 'setting'])->name('v2_setting');
+Route::get('v2/setting2', [V2UserController::class, 'setting2'])->name('v2_setting2');
