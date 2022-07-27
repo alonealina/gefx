79576504ceaa713554a99957946f555e8445011b
ksatake <k.satake.com@gmail.com>
2022-07-27 23:28:10 +0900

    test commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+// test
+
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
